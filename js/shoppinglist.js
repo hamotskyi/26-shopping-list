9b029a87b3610bd35b255e6f00c26c76880a526e
hamotskyi <a.plus.t.17.8.infinity@gmail.com>
2021-01-13 20:41:12 +0200

    fixed smth

--- js/shoppinglist.js
+++ js/shoppinglist.js
@@ -24,32 +24,39 @@ function addNewProduct() {
         let pQuantity = document.createElement('p');
         let inputCheck = document.createElement("input");
         let attFor = document.createAttribute("for");
-        let attOnchange = document.createAttribute("onchange");//
+        let attOnchange = document.createAttribute("onchange");
         let attIdForP = document.createAttribute("id");
         let attIdForInp = document.createAttribute("id");
         let attType = document.createAttribute("type");
-        let name = shoppingList[0].title
-        let quant = shoppingList[0].quantity
-        let textQuantity = document.createTextNode(`x ${quant}`)
-        let textName = document.createTextNode(`${name}`)
+        let name = shoppingList[0].title;
+        let quant = shoppingList[0].quantity;
+        let textQuantity = document.createTextNode(`x ${quant}`);
+        let textName = document.createTextNode(`${name}`);
+        let divX = document.createElement("div");//
+        let attOnclick = document.createAttribute("onclick");//
+        let attIdForDivX = document.createAttribute("id");//
         attType.value = "checkbox";
         attIdForInp.value = `check${shoppingList.length - 1}`;
         attFor.value = attIdForInp.value;
         attIdForP.value = `qt${shoppingList.length - 1}`;
-        attOnchange.value = "productFilter(this)";//
+        attOnchange.value = "productFilter(this)";
         ul.appendChild(li);
         li.appendChild(label);
+        li.appendChild(divX);//
         label.appendChild(pName);
         label.appendChild(pQuantity);
         label.appendChild(inputCheck);
+        attOnclick.value = "deleteListItem(this)";//
+        attIdForDivX.value = shoppingList.length - 1;//
+        divX.setAttributeNode(attOnclick);//
+        divX.setAttributeNode(attIdForDivX);//
         pName.appendChild(textName);
         pQuantity.appendChild(textQuantity);
         inputCheck.setAttributeNode(attType);
         inputCheck.setAttributeNode(attIdForInp);
-        inputCheck.setAttributeNode(attOnchange);//
+        inputCheck.setAttributeNode(attOnchange);
         label.setAttributeNode(attFor);
         pQuantity.setAttributeNode(attIdForP);
-
 
     } else {
 
@@ -70,17 +77,25 @@ function addNewProduct() {
             let attIdForP = document.createAttribute("id");
             let attIdForInp = document.createAttribute("id");
             let attType = document.createAttribute("type");
-            let name = shoppingList[shoppingList.length - 1].title
-            let quant = shoppingList[shoppingList.length - 1].quantity
-            let textQuantity = document.createTextNode(`x ${quant}`)
-            let textName = document.createTextNode(`${name}`)
+            let name = shoppingList[shoppingList.length - 1].title;
+            let quant = shoppingList[shoppingList.length - 1].quantity;
+            let textQuantity = document.createTextNode(`x ${quant}`);
+            let textName = document.createTextNode(`${name}`);
+            let divX = document.createElement("div");//
+            let attOnclick = document.createAttribute("onclick");//
+            let attIdForDivX = document.createAttribute("id");//
             attType.value = "checkbox";
             attIdForInp.value = `check${shoppingList.length - 1}`;
             attFor.value = attIdForInp.value;
             attIdForP.value = `qt${shoppingList.length - 1}`;
             attOnchange.value = "productFilter(this)";
+            attOnclick.value = "deleteListItem(this)";//
+            attIdForDivX.value = shoppingList.length - 1;//
+            divX.setAttributeNode(attOnclick);//
+            divX.setAttributeNode(attIdForDivX);//
             ul.appendChild(li);
             li.appendChild(label);
+            li.appendChild(divX);//
             label.appendChild(pName);
             label.appendChild(pQuantity);
             label.appendChild(inputCheck);
@@ -91,6 +106,7 @@ function addNewProduct() {
             inputCheck.setAttributeNode(attOnchange);
             label.setAttributeNode(attFor);
             pQuantity.setAttributeNode(attIdForP);
+
 
         } else {
 
@@ -120,4 +136,14 @@ function productFilter(x) {
         ul.appendChild(liParent)
     }
 
-}
+};
+
+function deleteListItem(y) {
+ 
+    let liParent = y.parentNode;
+    console.log(liParent);
+    liParent.remove();
+    let index = y.id;
+    shoppingList.splice(index, 1);
+    
+};
