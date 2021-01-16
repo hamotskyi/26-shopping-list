1c6eb46c1f1c04b60e737bd659bc7dd22f50fe2d
hamotskyi <a.plus.t.17.8.infinity@gmail.com>
2021-01-16 23:53:43 +0200

    plus minus 10%

--- js/shoppinglist.js
+++ js/shoppinglist.js
@@ -44,8 +44,9 @@ function addNewProduct() {
             let textQuantity = document.createTextNode(`x ${quant}`);
             let textName = document.createTextNode(`${name}`);
             let divX = document.createElement("div");
-            let attOnclick = document.createAttribute("onclick");
-            let attIdForDivX = document.createAttribute("id");
+            let attOnclickForX = document.createAttribute("onclick");
+            let attClassForX = document.createAttribute("class");
+            let attIdForLi = document.createAttribute("id");
             let placeholder = document.createAttribute("placeholder");
             attType.value = "checkbox";
             attIdForInp.value = `check${shoppingList.length - 1}`;
@@ -58,10 +59,12 @@ function addNewProduct() {
             label.appendChild(pName);
             label.appendChild(pQuantity);
             label.appendChild(inputCheck);
-            attOnclick.value = "deleteListItem(this)";
-            attIdForDivX.value = shoppingList.length - 1;
-            divX.setAttributeNode(attOnclick);
-            divX.setAttributeNode(attIdForDivX);
+            attOnclickForX.value = "deleteListItem(this)";
+            attIdForLi.value = shoppingList.length - 1;
+            attClassForX.value = 'divX';
+            divX.setAttributeNode(attOnclickForX);
+            divX.setAttributeNode(attClassForX);
+            li.setAttributeNode(attIdForLi);
             pName.appendChild(textName);
             pQuantity.appendChild(textQuantity);
             inputCheck.setAttributeNode(attType);
@@ -69,11 +72,24 @@ function addNewProduct() {
             inputCheck.setAttributeNode(attOnchange);
             label.setAttributeNode(attFor);
             pQuantity.setAttributeNode(attIdForP);
-
             placeholder.value = "Додай ще щось до списку";
             inp.setAttributeNode(placeholder);
             inp.value = "";
             inp.focus();
+
+            let divMinus = document.createElement("div");
+            let attClassForMinus = document.createAttribute("class");
+            divMinus.setAttributeNode(attClassForMinus);
+            attClassForMinus.value = 'divMinus';
+
+            let divPlus = document.createElement("div");
+            let attClassForPlus = document.createAttribute("class");
+            divPlus.setAttributeNode(attClassForPlus);
+            attClassForPlus.value = 'divPlus';
+
+            li.appendChild(divMinus);
+            li.appendChild(divPlus);
+
 
         } else {
 
@@ -99,18 +115,19 @@ function addNewProduct() {
                 let textQuantity = document.createTextNode(`x ${quant}`);
                 let textName = document.createTextNode(`${name}`);
                 let divX = document.createElement("div");
-                let attOnclick = document.createAttribute("onclick");
-                let attIdForDivX = document.createAttribute("id");
+                let attOnclickForX = document.createAttribute("onclick");
+            let attIdForDivX = document.createAttribute("id");
+                let attIdForLi = document.createAttribute("id");
                 let placeholder = document.createAttribute("placeholder");
                 attType.value = "checkbox";
                 attIdForInp.value = `check${shoppingList.length - 1}`;
                 attFor.value = attIdForInp.value;
                 attIdForP.value = `qt${shoppingList.length - 1}`;
                 attOnchange.value = "productFilter(this)";
-                attOnclick.value = "deleteListItem(this)";
-                attIdForDivX.value = shoppingList.length - 1;
-                divX.setAttributeNode(attOnclick);
-                divX.setAttributeNode(attIdForDivX);
+                attOnclickForX.value = "deleteListItem(this)";
+                attIdForLi.value = shoppingList.length - 1;
+                divX.setAttributeNode(attOnclickForX);
+                li.setAttributeNode(attIdForLi);
                 ul.appendChild(li);
                 li.appendChild(label);
                 li.appendChild(divX);
@@ -124,12 +141,60 @@ function addNewProduct() {
                 inputCheck.setAttributeNode(attOnchange);
                 label.setAttributeNode(attFor);
                 pQuantity.setAttributeNode(attIdForP);
-
                 placeholder.value = "Додай ще щось до списку";
                 inp.setAttributeNode(placeholder);
                 inp.value = "";
                 inp.focus();
 
+
+            } else if (shoppingList[index].quantity == 0) {
+
+                shoppingList[index].quantity++;
+                let li = document.createElement("li");
+                let label = document.createElement("label");
+                let pName = document.createElement('p');
+                let pQuantity = document.createElement('p');
+                let inputCheck = document.createElement("input");
+                let attFor = document.createAttribute("for");
+                let attOnchange = document.createAttribute("onchange");
+                let attIdForP = document.createAttribute("id");
+                let attIdForInp = document.createAttribute("id");
+                let attType = document.createAttribute("type");
+                let name = shoppingList[index].title;
+                let quant = shoppingList[index].quantity;
+                let textQuantity = document.createTextNode(`x ${quant}`);
+                let textName = document.createTextNode(`${name}`);
+                let divX = document.createElement("div");
+                let attOnclickForX = document.createAttribute("onclick");
+            let attIdForDivX = document.createAttribute("id");
+                let attIdForLi = document.createAttribute("id");
+                let placeholder = document.createAttribute("placeholder");
+                attType.value = "checkbox";
+                attIdForInp.value = `check${index}`;
+                attFor.value = attIdForInp.value;
+                attIdForP.value = `qt${index}`;
+                attOnchange.value = "productFilter(this)";
+                attOnclickForX.value = "deleteListItem(this)";
+                attIdForLi.value = index;
+                divX.setAttributeNode(attOnclickForX);
+                li.setAttributeNode(attIdForLi);
+                ul.appendChild(li);
+                li.appendChild(label);
+                li.appendChild(divX);
+                label.appendChild(pName);
+                label.appendChild(pQuantity);
+                label.appendChild(inputCheck);
+                pName.appendChild(textName);
+                pQuantity.appendChild(textQuantity);
+                inputCheck.setAttributeNode(attType);
+                inputCheck.setAttributeNode(attIdForInp);
+                inputCheck.setAttributeNode(attOnchange);
+                label.setAttributeNode(attFor);
+                pQuantity.setAttributeNode(attIdForP);
+                placeholder.value = "Додай ще щось до списку";
+                inp.setAttributeNode(placeholder);
+                inp.value = "";
+                inp.focus();
 
             } else {
 
@@ -138,7 +203,6 @@ function addNewProduct() {
                 let quant = shoppingList[index].quantity;
                 let textQuantity = document.createTextNode(`x ${quant}`);
                 let placeholder = document.createAttribute("placeholder");
-
                 pQ.removeChild(pQ.firstChild);
                 pQ.appendChild(textQuantity);
                 placeholder.value = "Додай ще щось до списку";
@@ -170,9 +234,8 @@ function productFilter(x) {
 function deleteListItem(y) {
  
     let liParent = y.parentNode;
-    console.log(liParent);
+    let index = liParent.id;
     liParent.remove();
-    let index = y.id;
-    shoppingList.splice(index, 1);
+    shoppingList[index].quantity = 0;
     
 };
